91b995244d15cc88d10f46506e1a22198cac7bcb
eitanweiss <eitan.w@campus.technion.ac.il>
2024-08-12 12:42:02 +0300

    editView V-0.9
     - transfer from collection to saved deck and back by clicking
     - added a ViewClick component to the cards
     - removed the Clickable component from the cards
     - added a save button which saves the changes to the json files (provides concurrency between game runs)

--- Assets/EditView.cs
+++ Assets/EditView.cs
@@ -10,7 +10,7 @@ public class EditView : MonoBehaviour
     [SerializeField]
     private Transform collectionViewContent;
     [SerializeField]
-    private Transform SavedDeckViewContent;
+    private Transform savedDeckViewContent;
 
     [SerializeField]
     private GameObject prefab;
@@ -27,36 +27,35 @@ public class EditView : MonoBehaviour
         //load saved deck from json
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    private void LoadSavedDeck()
+    public void Restart()
     {
-        savedDeck = new List<CardScriptableObject>();
-        string path = Path.Combine(Application.dataPath, "Saved.json");
-        if (File.Exists(path))
+        //empty children from content view
+        for(int i=collectionViewContent.childCount-1;i>=0 ;i--)
         {
-            string json = File.ReadAllText(path);
-            SerializableList<SerializableCard> loadedData = JsonUtility.FromJson<SerializableList<SerializableCard>>(json);
-            foreach (var cardData in loadedData.list)
-            {
-                savedDeck.Add(cardData.ToScriptableObject());
-            }
+            Destroy(collectionViewContent.GetChild(i).gameObject);
         }
-        foreach(CardScriptableObject card in savedDeck)
+        for(int i=savedDeckViewContent.childCount-1;i>=0 ;i--)
         {
-            GameObject cardGO = Instantiate(prefab, SavedDeckViewContent);
-            CardObject newCardObj = cardGO.AddComponent<CardObject>();
-            newCardObj.card = card;
-            DisplayCard displayCard = cardGO.GetComponent<DisplayCard>();
-            displayCard.SetCard(card);
-            Debug.Log("Card up");
+            Destroy(savedDeckViewContent.GetChild (i).gameObject);
         }
-        Debug.Log("All Cards are up");
+        LoadCollection();
+        LoadSavedDeck();
+    }
+
+    public void SaveCollection()
+    {
+        List<SerializableCard> serializedCards= new List<SerializableCard>();
+        foreach(var card in collection)
+        {
+            serializedCards.Add(new SerializableCard(card));
+        }
+        string json = JsonUtility.ToJson(new SerializableList<SerializableCard>(serializedCards), true);
+        string path = Path.Combine(Application.dataPath, "Collection.json");
+        File.WriteAllText(path, json);
     }
 
     /// <summary>
-    /// 
+    /// load the current collection
     /// </summary>
     private void LoadCollection()
     {
@@ -81,10 +80,108 @@ public class EditView : MonoBehaviour
             newCardObj.card = card;
             DisplayCard displayCard = cardGO.GetComponent<DisplayCard>();
             displayCard.SetCard(card);
+            cardGO.AddComponent<ViewClick>();
+            Destroy(cardGO.GetComponent<Clickable>());
             Debug.Log("Card up");
         }
         Debug.Log("All Cards are up");
     }
     //when clicked - move from one to the other
-    //limit the number of cards in the saved deck according to 
+
+    /// <summary>
+    /// add CardObject card to collection
+    /// </summary>
+    /// <param name="card">card to add</param>
+    public void AddToCollection(CardObject card)
+    {
+        collection.Add(card.card);
+    }
+
+    /// <summary>
+    /// remove CardScriptableObject card from colleciton
+    /// </summary>
+    /// <param name="card">card to be removed from collection</param>
+    public void RemoveFromCollection(CardObject card)
+    {
+        collection.Remove(card.card);
+    }
+
+    /// <summary>
+    /// get the transform of collection content
+    /// </summary>
+    /// <returns> transform of collection content</returns>
+    public Transform GetCollectionTransform()
+    {
+        return collectionViewContent;
+    }
+
+    /// <summary>
+    /// add CardObject card to savedDeck
+    /// </summary>
+    /// <param name="card">card to add</param>
+    public void AddToSaved(CardObject card)
+    {
+        savedDeck.Add(card.card);
+    }
+
+    /// <summary>
+    /// remove card from saved deck
+    /// </summary>
+    /// <param name="card">card to be removed from collection</param>
+    public void RemoveFromSaved(CardObject card)
+    {
+        savedDeck.Remove(card.card);
+    }
+
+    public void SaveSavedDeck()
+    {
+        List<SerializableCard> serializedCards = new List<SerializableCard>();
+        foreach (var card in savedDeck)
+        {
+            serializedCards.Add(new SerializableCard(card));
+        }
+        string json = JsonUtility.ToJson(new SerializableList<SerializableCard>(serializedCards), true);
+        string path = Path.Combine(Application.dataPath, "Saved.json");
+        File.WriteAllText(path, json);
+    }
+
+    /// <summary>
+    /// load the current saved deck
+    /// </summary>
+    private void LoadSavedDeck()
+    {
+        savedDeck = new List<CardScriptableObject>();
+        string path = Path.Combine(Application.dataPath, "Saved.json");
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SerializableList<SerializableCard> loadedData = JsonUtility.FromJson<SerializableList<SerializableCard>>(json);
+            foreach (var cardData in loadedData.list)
+            {
+                savedDeck.Add(cardData.ToScriptableObject());
+            }
+        }
+        foreach (CardScriptableObject card in savedDeck)
+        {
+            GameObject cardGO = Instantiate(prefab, savedDeckViewContent);
+            CardObject newCardObj = cardGO.AddComponent<CardObject>();
+            newCardObj.card = card;
+            DisplayCard displayCard = cardGO.GetComponent<DisplayCard>();
+            displayCard.SetCard(card);
+            cardGO.AddComponent<ViewClick>();
+            Destroy(cardGO.GetComponent<Clickable>());
+            Debug.Log("Card up");
+        }
+        Debug.Log("All Cards are up");
+    }
+
+    /// <summary>
+    /// get the transform of savedDeck content
+    /// </summary>
+    /// <returns>transform of savedDeck content</returns>
+    public Transform GetSavedTransform()
+    {
+        return savedDeckViewContent;
+    }
+    //limit the number of cards in the saved deck according to - higher order not important ATM
 }
---
+++ Assets/ViewClick.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ViewClick : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        GameObject editView = transform.parent.parent.parent.parent.gameObject;
+        if(transform.parent.name == "CollectionContent")
+        {
+            //move to Saved/sell cards
+            //remove from collection
+            Debug.Log("parent is collection");
+            editView.GetComponent<EditView>().RemoveFromCollection(eventData.pointerClick.GetComponent<CardObject>());
+            //move to saved
+            transform.SetParent(editView.GetComponent<EditView>().GetSavedTransform());
+            //add to saved
+            editView.GetComponent<EditView>().AddToSaved(eventData.pointerClick.GetComponent<CardObject>());
+        }
+        else
+        {
+            //move to collection
+            editView.GetComponent<EditView>().RemoveFromSaved(eventData.pointerClick.GetComponent<CardObject>());
+            //move to saved
+            transform.SetParent(editView.GetComponent<EditView>().GetCollectionTransform());
+            //add to saved
+            editView.GetComponent<EditView>().AddToCollection(eventData.pointerClick.GetComponent<CardObject>());
+        }
+    }
+}
